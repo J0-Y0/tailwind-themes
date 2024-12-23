e834811894e20dd17c085c88048d3030490d66f2
yosef <yosfemyayu@gmail.com>
2024-12-23 13:03:52 -0800

    tailwind  custom Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,27 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
 
   theme: {
+    // fontFamily: {
+    //   sans: ["sans-serif"],
+    //   // serif: ["Merriweather", "serif"],
+    // },
+    colors: {
+      primary: "#147294",
+      secondary: "#F4C236",
+      tertiary: "#E6E6E6",
+      quaternary: "#1A202C",
+      quinary: "#F4F4F4",
+      success: "#2ECC71",
+      info: "#3498DB",
+      warning: "#FFC107",
+      danger: "#E74C3C",
+      // light: "#F8F9FA",
+      // dark: "#343A40",
+
+      bg_dark: "#0D1F2D",
+      text_light: "#66d6ff",
+    },
+
     extend: {},
   },
   plugins: [],
